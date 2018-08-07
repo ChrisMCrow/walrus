262a30216eb014f9fbc7f3c0a085bd4a87ddb2b4
catherine bradley and Chris Crow <chris@>
2018-08-07 09:09:28 -0700

    not fade the p elements while keeping slide transition on images.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#img-hidden").fadeToggle();
-    $("#img-showing").fadeToggle();
+    $("#img-hidden").slideToggle();
+    $("#img-showing").slideToggle();
+  });
+
+  $("#img-hidden p").click(function() {
+    $("#img-hidden").toggle();
+  });
+
+  $("#img-showing p").click(function() {
+    $("#img-showing").toggle();
   });
 });
